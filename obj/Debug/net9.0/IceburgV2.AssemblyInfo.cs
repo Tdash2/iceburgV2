37066193be685c6e9a82c7e6f5025ddc4777398d
Tdash2 <69229823+Tdash2@users.noreply.github.com>
2026-09-10 18:44:49 -0500

    Updated Router

--- obj/Debug/net9.0/IceburgV2.AssemblyInfo.cs
+++ obj/Debug/net9.0/IceburgV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IceburgV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9454e7242cdfbf6f94aacf9ebe9a43f34440f32b")]
 [assembly: System.Reflection.AssemblyProductAttribute("IceburgV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IceburgV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
